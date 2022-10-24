63fff9c1d23b52b2a796a9c5182e0a69b37b4cd1
Murf-y <charbelfayad64@gmail.com>
2022-10-24 18:26:27 +0300

    add view binding

--- App/app/src/main/java/com/murfy/groupify/activities/SplashScreen.java
+++ App/app/src/main/java/com/murfy/groupify/activities/SplashScreen.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.murfy.groupify.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
--- App/app/src/main/java/com/murfy/groupify/utils/AnimationHelper.java
+++ App/app/src/main/java/com/murfy/groupify/utils/AnimationHelper.java
@@ -22,14 +22,6 @@ public class AnimationHelper {
         return animationHelper;
     }
 
-    public AnimationHelper slideFromLeftToRight(View v, int duration){
-        float x = v.getX();
-        v.setX(-1000);
-        v.animate().x(x).setDuration(duration);
-
-        return getInstance();
-    }
-
     public AnimationHelper fadeIn(View v, int duration){
         v.setAlpha(0);
         v.animate().alpha(1).setDuration(duration);
