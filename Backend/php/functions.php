062e31b019cf57fee3402ce7a2ef1ced42c7e867
EliasXabs <eliasxabs@gmail.com>
2022-10-24 22:15:12 +0300

    Function that checks matching pass

--- Backend/php/functions.php
+++ Backend/php/functions.php
@@ -28,3 +28,16 @@ function emailExists($connection, $email){
     return mysqli_fetch_assoc($results);
 
 }
+
+function passmatch($connection, $pass, $username){
+    $sql = "SELECT password FROM users WHERE username = ?";
+
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $results = mysqli_stmt_get_result($stmt);
+
+    return $row = mysql_fetch_array($result) && $row["password"] == $pass;
+}
