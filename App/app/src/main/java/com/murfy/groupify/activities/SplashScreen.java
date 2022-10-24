c6d78dc3441359949c798f48db2677d0ae8769b1
Murf-y <charbelfayad64@gmail.com>
2022-10-24 18:34:09 +0300

    added binding :rocket:

--- App/app/src/main/java/com/murfy/groupify/activities/SplashScreen.java
+++ App/app/src/main/java/com/murfy/groupify/activities/SplashScreen.java
@@ -3,14 +3,17 @@ package com.murfy.groupify.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.murfy.groupify.R;
+import com.murfy.groupify.databinding.ActivitySplashScreenBinding;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        ActivitySplashScreenBinding binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
     }
 }
