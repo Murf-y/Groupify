621b0f85101009d9006813da4f91ec25eb7940b5
Murf-y <charbelfayad64@gmail.com>
2022-11-03 12:48:39 +0200

    add home page + profile page

--- App/app/src/main/java/com/murfy/groupify/activities/HomeActivity.java
+++ App/app/src/main/java/com/murfy/groupify/activities/HomeActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.gson.Gson;
 import com.murfy.groupify.R;
 import com.murfy.groupify.databinding.ActivityHomeBinding;
+import com.murfy.groupify.models.User;
 
 public class HomeActivity extends AppCompatActivity {
 
     private boolean showingMyGroups = true;
     private ActivityHomeBinding binding;
+
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,14 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View root = binding.getRoot();
         setContentView(root);
+
+        currentUser = new Gson().fromJson(getSharedPreferences("groupify", MODE_PRIVATE).getString("current_user", "{}"), User.class);
+
+        binding.avatar.setImageBitmap(currentUser.getProfilePhoto());
+        binding.avatar.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+            startActivity(i);
+        });
 
         binding.mygroups.setOnClickListener(view -> changeSection(view, true));
         binding.explore.setOnClickListener(view -> changeSection(view, false));
@@ -42,6 +56,7 @@ public class HomeActivity extends AppCompatActivity {
             showingMyGroups = true;
             updateListView();
         }
+
         if(showingMyGroups && !shouldShowMyGroups){
             binding.mygroups.setTextColor(getResources().getColor(R.color.grey_200));
             binding.explore.setTextColor(getResources().getColor(R.color.dark_blue_700));
