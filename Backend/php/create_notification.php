d59da885faceade30bd6ea6f67c02e5452294f0c
EliasXabs <eliasxabs@gmail.com>
2022-11-06 11:50:34 +0200

    Update post notification

--- Backend/php/create_notification.php
+++ Backend/php/create_notification.php
@@ -6,3 +6,11 @@ $user_id = $_POST["user_id"];
 $message = $_POST["message"];
 $group_id = $_POST["group_id"];
 
+$query = $connection -> prepare("SELECT user_id FROM group_members WHERE group_id = ?");
+$query->bind_param("i", $group_id);
+$query->execute();
+$result = $query->get_result();
+
+while($ele = $result->fetch_assoc()){
+    $users[] = $ele;
+}
